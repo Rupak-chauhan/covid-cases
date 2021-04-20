625416598e0f03339937187d68b7d9d9dd759480
rupak singh chauhan <rupakchauhan835@gmail.com>
2021-04-20 09:13:46 +0530

    Updates

---
+++ precache-manifest.0884ea8ac9a40d728a415289972b1e30.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49735801577df9361dd8603ecb0dcf32",
+    "url": "/covid-cases/index.html"
+  },
+  {
+    "revision": "e0e94761a6f353a0d05d",
+    "url": "/covid-cases/static/css/2.f274bf3c.chunk.css"
+  },
+  {
+    "revision": "d777b3a74f8dcee855e3",
+    "url": "/covid-cases/static/css/main.d714c91f.chunk.css"
+  },
+  {
+    "revision": "e0e94761a6f353a0d05d",
+    "url": "/covid-cases/static/js/2.5fd04cc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-cases/static/js/2.5fd04cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d777b3a74f8dcee855e3",
+    "url": "/covid-cases/static/js/main.fa7d331a.chunk.js"
+  },
+  {
+    "revision": "79c99b3905ee06a056f6",
+    "url": "/covid-cases/static/js/runtime-main.2e25a72a.js"
+  },
+  {
+    "revision": "149c31c145bc4ec016f5d57a36836899",
+    "url": "/covid-cases/static/media/covi.149c31c1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid-cases/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid-cases/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid-cases/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid-cases/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid-cases/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
